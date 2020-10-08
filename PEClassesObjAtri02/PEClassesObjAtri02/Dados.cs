da5e74266b2b784f747ee250c74d2f6aed76afea
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2020-10-07 22:58:27 -0300

    Classe, Objetos e Atributos - Exer02
    Rio de Janeiro
    Quarta-feira dia 07-10-2020
    Aula 40 - Primeiros exercícios (classes, objetos e atributos).
    Exercício 02
    Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário médio dos funcionários.
    Exemplo:
    Dados do primeiro funcionário:
    Nome: Carlos Silva
    Salário: 6300.00
    Dados do segundo funcionário:
    Nome: Ana Marques
    Salário: 6700.00
    Salário médio = 6500.00

---
+++ PEClassesObjAtri02/PEClassesObjAtri02/Dados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PEClassesObjAtri02
+{
+    class Dados
+    {
+        public string Nom;
+        public double SaLr;
+    }
+}
---
+++ PEClassesObjAtri02/PEClassesObjAtri02/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Rio de Janeiro
+ * Quarta-feira dia 07-10-2020
+ * Aula 40 - Primeiros exercícios (classes, objetos e atributos).
+ * Exercício 02
+ * Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário médio dos funcionários.
+ * Exemplo:
+ * Dados do primeiro funcionário:
+ * Nome: Carlos Silva
+ * Salário: 6300.00
+ * Dados do segundo funcionário:
+ * Nome: Ana Marques
+ * Salário: 6700.00
+ * Salário médio = 6500.00 */
+
+using System;
+using System.Globalization;
+
+namespace PEClassesObjAtri02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dados Nom, SaLr;
+
+            Nom = new Dados();
+            SaLr = new Dados();
+
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            String Nome01 = Console.ReadLine();
+            Console.Write("Salário: ");
+            double Salario01 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.Write("Nome: ");
+            String Nome02 = Console.ReadLine();
+            Console.Write("Salário: ");
+            double Salario02 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double Resultado = (Salario01 + Salario02) / 2.0;
+            Console.WriteLine("Salário médio = " + Resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
